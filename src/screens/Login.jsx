17058d83ce60b8831a10da620207431676edb184
kunal Lingwal <kunallingwal2@gmail.com>
2024-02-21 00:13:12 +0530

    Update Login.jsx

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -19,6 +19,7 @@ export default function Login() {
     );
     const response = await fetch("https://nom-nom-backend.vercel.app/api/login", {
       method: "POST",
+      withCredentials:"false",
       headers: {
         "Content-Type": "application/json",
       },
